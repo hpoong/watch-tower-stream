88614f6cb4e9fcbc0562d86d4ee9d31ccf5dbf8d
ho_poong <hopoong23@gmail.com>
2025-05-09 18:25:26 +0900

    refactor(audit-service): 사용되지 않는 코드 제거

--- audit-service/src/main/java/com/hopoong/audit/adapter/kafka/DlqRecoveryConsumer.java
+++ audit-service/src/main/java/com/hopoong/audit/adapter/kafka/DlqRecoveryConsumer.java
@@ -10,16 +10,11 @@ import com.hopoong.core.message.resourcemonitor.SystemResourceMetricsMessage;
 import com.hopoong.core.topic.KafkaTopicManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.time.Duration;
 
 @Slf4j
@@ -30,50 +25,7 @@ public class DlqRecoveryConsumer {
 
     private final ObjectMapper objectMapper;
 
-//    private final KafkaTemplate<String, Object> kafkaTemplate;
-
     private final ResourceMonitorService resourceMonitorService;
-//
-//
-//    @KafkaListener(
-//            topics = KafkaTopicManager.SYSTEM_RESOURCE_METRICS_TOPIC + ".DLQ",
-//            groupId = "system-resource-metrics-dlq-group",
-//            containerFactory = "kafkaListenerContainerSystemMetricsFactory",
-//            concurrency = "1"
-//    )
-//    public void systemResourceMetricsRecover(ConsumerRecord<String, String> record, Acknowledgment ack) {
-//        try {
-//            if(!isRetryable(record.value())) {
-//                KafkaCommonMessage<SystemResourceMetricsMessage> message =
-//                        objectMapper.readValue(record.value(), new TypeReference<>() {});
-//
-//                String partitionKey = message.getBody().serverName();
-//                kafkaTemplate.send(KafkaTopicManager.SYSTEM_RESOURCE_METRICS_TOPIC, partitionKey, record.value());
-//
-////                ack.acknowledge();
-//
-//                System.out.println(message.getHeader().getTraceId());
-//                System.out.println("중복 아님");
-//            } else {
-//                // DB 저장 or traceId 기반 중복 방지용 Redis
-//                System.out.println("중복 ");
-//            }
-//
-//        } catch (Exception e) {
-//            log.error("DLQ 처리 실패", e);
-//        }
-//    }
-//
-//    private boolean isRetryable(String value) throws IOException, InterruptedException {
-//        KafkaCommonMessage<SystemResourceMetricsMessage> message =
-//                objectMapper.readValue(value, new TypeReference<>() {});
-//
-//        String traceId = message.getHeader().getTraceId();
-//
-//        return resourceMonitorService.existsByTraceId(traceId);
-//    }
-
-
 
     @Bean
     public KStream<String, String> dlqDelayStream(StreamsBuilder builder) {
