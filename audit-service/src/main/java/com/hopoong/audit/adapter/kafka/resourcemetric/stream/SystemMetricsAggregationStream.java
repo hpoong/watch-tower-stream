acf4c6499d596340f733e5fe00e659e4374f9c81
ho_poong <hopoong23@gmail.com>
2025-06-25 16:45:54 +0900

    feat(audit-service) - 시스템 리소스 사용률의 5분 avg, min, max 값 Kafka Stream 개발 완료

--- audit-service/src/main/java/com/hopoong/audit/adapter/kafka/resourcemetric/stream/SystemMetricsAggregationStream.java
+++ audit-service/src/main/java/com/hopoong/audit/adapter/kafka/resourcemetric/stream/SystemMetricsAggregationStream.java
@@ -18,6 +18,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Component
@@ -26,10 +29,25 @@ public class SystemMetricsAggregationStream {
 
     private final ObjectMapper objectMapper;
 
+    @Bean
+    public KStream<String, String> cpu5minAvgMaxStream(StreamsBuilder builder) {
+        return build5minAvgMaxStream(builder, "CPU");
+    }
+
+    @Bean
+    public KStream<String, String> disk5minAvgMaxStream(StreamsBuilder builder) {
+        return build5minAvgMaxStream(builder, "DISK");
+    }
+
+    @Bean
+    public KStream<String, String> memory5minAvgMaxStream(StreamsBuilder builder) {
+        return build5minAvgMaxStream(builder, "Memory");
+    }
+
     /*
      * 리소스 사용량 5분 평균 값 및 min, max 값 조회
      */
-    public KStream<String, String> buildAvgMaxStream(StreamsBuilder builder, String type) {
+    public KStream<String, String> build5minAvgMaxStream(StreamsBuilder builder, String type) {
 
         // system-resource-metrics : KStream
         GenericJsonSerde<KafkaCommonMessage<SystemResourceMetricsMessage>> resourceSerde =
@@ -40,28 +58,52 @@ public class SystemMetricsAggregationStream {
                 Consumed.with(Serdes.String(), resourceSerde)
         );
 
-        KStream<String, KafkaCommonMessage<SystemResourceMetricsMessage>> cpuStream =
+        KStream<String, KafkaCommonMessage<SystemResourceMetricsMessage>> stream =
             resourceMetricsStream.filter((key, value) ->
-                    value.getBody().resourceName().equalsIgnoreCase("CPU")
+                    value.getBody().resourceName().equalsIgnoreCase(type)
             );
 
+        stream.foreach((key, value) -> {
+            LoggerUtil.section(log, "[data stream] key = %s, serverName = %s, value = %s".formatted(key, value.getBody().serverName(), value.getBody().usagePercent()));
+        });
 
         TimeWindows oneMinute = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1));
         TimeWindows fiveMinutes = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(5));
         TimeWindows tenMinutes = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(10));
 
         // 5분 평균
-        KTable<Windowed<String>, AvgMax> avgMaxOneMin = cpuStream
+        KTable<Windowed<String>, AvgMax> avgMaxOneMin = stream
             .groupByKey()
             .windowedBy(fiveMinutes)
             .aggregate(
                     AvgMax::new,
                     (key, value, aggregate) -> aggregate.add(value.getBody().usagePercent()),
                     Materialized.with(Serdes.String(), new AvgMaxSerde(objectMapper))
-            );
+            )
+            .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded()));
+
 
         avgMaxOneMin.toStream().foreach((windowedKey, value) -> {
-            LoggerUtil.section(log, "[5분] CPU 평균 = %.2f, 최대 = %.2f".formatted(value.avg(), value.max()));
+
+            long startEpoch = windowedKey.window().start();
+            long endEpoch = windowedKey.window().end();
+
+            String startTime = Instant.ofEpochMilli(startEpoch)
+                    .atZone(ZoneId.systemDefault())
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            String endTime = Instant.ofEpochMilli(endEpoch)
+                    .atZone(ZoneId.systemDefault())
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+            LoggerUtil.section(log, """
+                [%s][%s] 5분 평균 = %.2f, 최대 = %.2f
+                윈도우 시작: %s
+                윈도우 종료: %s
+                """.formatted(
+                            windowedKey.key(), type,
+                            value.avg(), value.max(),
+                            startTime, endTime
+                    ));
         });
 
         return null;
