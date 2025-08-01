235c3893b1911627bd375bca673d37ba54c7af63
ho_poong <hopoong23@gmail.com>
2025-08-01 16:14:43 +0900

    refactor(ai-analytics-service): 불필요한 코드 제거

--- ai-analytics-service/usecase/anomaly_detector/fetcher.py
+++ ai-analytics-service/usecase/anomaly_detector/fetcher.py
@@ -1,7 +1,7 @@
-from elasticsearch import Elasticsearch
-from usecase.anomaly_detector.query_builder import build_query
-from datetime import datetime, timedelta
 import pandas as pd
+from elasticsearch import Elasticsearch
+
+from usecase.anomaly_detector.query_builder import build_query
 
 
 def fetch_usage(
@@ -41,7 +41,6 @@ def fetch_usage_dataframe(
     query = build_query(server, resource, minutes)
     try:
         res = es.search(index=index, body=query)
-        print(res)
         usage_list = [
             {
                 "timestamp": hit["_source"]["timestamp"],
@@ -49,7 +48,6 @@ def fetch_usage_dataframe(
             }
             for hit in res["hits"]["hits"]
         ]
-        print(usage_list)
         df = pd.DataFrame(usage_list)
         df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True)
         return df
