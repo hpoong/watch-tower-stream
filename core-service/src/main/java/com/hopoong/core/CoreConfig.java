56bd1a0bc848d0c952cddc7dbef71d49bbc9ccdd
ho_poong <hopoong23@gmail.com>
2025-08-11 15:23:24 +0900

    feat(audit-service): core-service 설정(application-core.yml) 로드 가능하도록 구성 수정

--- core-service/src/main/java/com/hopoong/core/CoreConfig.java
+++ core-service/src/main/java/com/hopoong/core/CoreConfig.java
@@ -1,9 +1,26 @@
 package com.hopoong.core;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import com.hopoong.core.index.ElasticsearchIndexProperties;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class CoreConfig { }
+@Configuration
+@EnableConfigurationProperties(ElasticsearchIndexProperties.class)
+public class CoreConfig implements CommandLineRunner {
+
+    private final Environment env;
+
+    public CoreConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("==== application-core.yml direct read test ====");
+        System.out.println("systemMetrics => " + env.getProperty("elasticsearch.index.system-metrics"));
+        System.out.println("systemMetrics5Min => " + env.getProperty("elasticsearch.index.system-metrics-5min"));
+        System.out.println("==========================================");
+    }
+}
--- core-service/src/main/java/com/hopoong/core/index/ElasticsearchIndexProperties.java
+++ core-service/src/main/java/com/hopoong/core/index/ElasticsearchIndexProperties.java
@@ -1,11 +1,11 @@
 package com.hopoong.core.index;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
-@Component
+@Setter
 @ConfigurationProperties(prefix = "elasticsearch.index")
 public class ElasticsearchIndexProperties {
     private String systemMetrics;
