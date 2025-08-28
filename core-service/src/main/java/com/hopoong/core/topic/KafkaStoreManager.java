cedf724f85637a9345f6783d921b2d81b9ec406a
ho_poong <hopoong23@gmail.com>
2025-08-28 19:16:18 +0900

    feat(core-service): [store] - user-service-feature-counts-store 추가

--- core-service/src/main/java/com/hopoong/core/topic/KafkaStoreManager.java
+++ core-service/src/main/java/com/hopoong/core/topic/KafkaStoreManager.java
@@ -2,4 +2,6 @@ package com.hopoong.core.topic;
 
 public class KafkaStoreManager {
     public static final String SYSTEM_RESOURCE_METRICS_ERROR_COUNT_STORE = "system-resource-metrics-error-count-store";
+
+    public static final String USER_SERVICE_FEATURE_COUNTS_STORE = "user-service-feature-counts-store";
 }
